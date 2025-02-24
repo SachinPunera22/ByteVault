66ba94d0e0b674794cd7b61ede58b078a13193ad
meenakshibhatt <meenakshibhatt844@gmail.com>
2025-02-24 16:59:34 +0530

    update message service to send and receive commands

--- client/utils/health.service.ts
+++ client/utils/health.service.ts
@@ -1,23 +1,28 @@
-import type {Socket, TCPSocket} from "bun";
-import type {MessageService} from "../message.service.ts";
-import {systemEventService} from "../events/systemEvent.service.ts";
+import type { Socket, TCPSocket } from "bun";
+import type { MessageService } from "../message.service.ts";
+import { systemEventService } from "../events/systemEvent.service.ts";
 import LoggerService from "./logger-service.ts";
+import {ClientCommands, ServerCommands } from "../constants.ts";
 
 export class HealthService {
+  constructor(
+    protected socket: TCPSocket,
+    protected messageService: MessageService
+  ) {
+    this.setupListener();
+  }
 
-    constructor(protected socket: TCPSocket, protected messageService: MessageService) {
-        this.setupListener();
-    }
+  public checkConnection() {
+    return this.messageService.send(
+      { command: ClientCommands.PING, message: Buffer.from("ping") },
+      this.socket
+    );
+  }
 
-    public  checkConnection() {
-        return this.messageService.send({command: 'ping', message: Buffer.from('ping')}, this.socket)
-    }
-
-    public async setupListener() {
-        systemEventService.on('ping', () => this.checkConnection)
-        systemEventService.on('pong', () => {
-            LoggerService.success('pong')
-        })
-
-    }
+  public async setupListener() {
+    systemEventService.on("ping", () => this.checkConnection);
+    systemEventService.on(ServerCommands.PONG, () => {
+      LoggerService.success("pong");
+    });
+  }
 }
--- server/constants.ts
+++ server/constants.ts
@@ -1,2 +1,19 @@
-export const Start = "55";
-export const End = "AA";
+export enum ServerCommands {
+  PONG="pong"
+}
+
+export enum ClientCommands {
+  PING="ping"
+}
+
+export enum StatusByte {
+  OK = "00",
+  ERROR = "01",
+  End = "81",
+}
+
+export enum ClientStatusByte {
+  START = "00",
+  END = "81",
+}
+
