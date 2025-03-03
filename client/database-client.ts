cd8406f2fc7a893f9c4e38a74c215268da0178dc
BharteshDhyani <bharteshdhyani10@gmail.com>
2025-03-03 15:03:44 +0530

    Dynamic server connection by taking host, port and password at client startup arguments

--- client/database-client.ts
+++ client/database-client.ts
@@ -1,36 +1,35 @@
-import type {Socket, TCPSocket} from "bun";
-import {ClientConfiguration} from "./config/config";
-import {MessageService} from "./message.service.ts";
+import type { Socket, TCPSocket } from "bun";
+import { ClientConfiguration } from "./config/config";
 import LoggerService from "./utils/logger-service";
-import {AuthenticationService} from "./authentication/authentication-service";
-import {EventEmitter} from "events";
 
 export class DatabaseClient {
-    public client!: TCPSocket;
+  public client!: TCPSocket;
+  public configService: ClientConfiguration;
+  constructor() {
+    this.configService = new ClientConfiguration();
+  }
 
+  /**
+   * Starts the client connection to the server
+   * @returns {Promise<TCPSocket>}
+   */
+  public async connectToServer(socket: Socket, cli_arguments: { host: string; port: number; password: string }): Promise<TCPSocket> {
+    try {
+        const storedConfig= this.configService.readConfig();
+        this.configService.set('cli_arguments',cli_arguments)
 
-    constructor() {
+      this.client = await Bun.connect({
+        hostname: cli_arguments.host,
+        port: cli_arguments.port,
+        socket: socket as any,
+      });
+
+      LoggerService.success(`Connected to server at ${cli_arguments.host}:${cli_arguments.port}`);
+      return this.client;
+    } catch (error) {
+      LoggerService.error(`Failed to connect to server: ${error}`);
+      throw error;
     }
+  }
 
-    /**
-     * Starts the client connection to the server
-     * @returns {Promise<void>}
-     */
-    public async connectToServer(socket:Socket): Promise<TCPSocket> {
-        try {
-            const config = new ClientConfiguration();
-            await config.readConfig(); // Load configuration file
-
-            const hostname: string = config.get("hostname", "localhost");
-            const port: number = Number(config.get("port", "4000"));
-            this.client = await Bun.connect({
-                hostname,
-                port,
-                socket
-            });
-            return this.client;
-        } catch (error) {
-            LoggerService.error(`Failed to connect to server: ${error}`);
-        }
-    }
 }
